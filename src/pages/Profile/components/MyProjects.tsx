e8978b2e702cd94c2868cf96a886af8b254b6a18
lyelyaa <koziyolga16@gmail.com>
2023-12-25 10:12:32 +0500

    small fix myprojects

--- src/pages/Profile/components/MyProjects.tsx
+++ src/pages/Profile/components/MyProjects.tsx
@@ -10,112 +10,131 @@ import CheckedProjectTeamBlock from "../../../components/CheckedProjectTeamBlock
 
 
 interface ButtonSeasonProps {
-  label: string,
-  disable?: boolean,
-  select: boolean,
+    label: string,
+    disabled?: boolean,
+    select: boolean,
 }
 
-function ButtonSeason({label = '', disable = false, select}: ButtonSeasonProps) {
-  let color
-  if (select) {
-    color = '#5A9DF5'
-  } else {
-    color = '#282828'
-  }
-  return (
-      <>
-        {disable ? (<ButtonSeasonStyle disabled={true}>{label}</ButtonSeasonStyle>)
-            : (<ButtonSeasonStyle style=
-                                      {{
-                                        backgroundColor: color,
-                                      }}>{label}</ButtonSeasonStyle>)}
-      </>
-  )
+function ButtonSeason({label = '', disabled = false, select}: ButtonSeasonProps) {
+    let color
+    if (select) {
+        color = '#5A9DF5'
+    } else {
+        color = '#282828'
+    }
+    return (
+        <>
+            {disabled ? (<ButtonSeasonStyle disabled={true}>{label}</ButtonSeasonStyle>)
+                : (<ButtonSeasonStyle style=
+                                          {{
+                                              backgroundColor: color,
+                                          }}>{label}</ButtonSeasonStyle>)}
+        </>
+    )
 }
 
 const MyProjects = () => {
-  const [activeButton, setActiveButton] = useState(0)
+    const [activeButton, setActiveButton] = useState(0)
 
-  // @ts-ignore
-  const {SetLabel} = useContext(Context)
-  const {data} = useData()
+    // @ts-ignore
+    const {SetLabel} = useContext(Context)
+    const {data} = useData()
 
-  const seasons = ['Осень 2022', 'Весна 2022', 'Осень 2023', 'Весна 2023']
+    const seasons = [
+        {
+            label: 'Осень 2022',
+            disabled: false
+        },
+        {
+            label: 'Весна 2022',
+            disabled: false
+        },
+        {
+            label: 'Осень 2023',
+            disabled: false
+        },
+        {
+            label: 'Весна 2023',
+            disabled: true
+        }
+    ]
 
-  useEffect(() => {
-    SetLabel('Мои проекты')
-  }, []);
+    useEffect(() => {
+        SetLabel('Мои проекты')
+    }, []);
 
-  return (
-      <MyProjectStyle>
-        <H2Style style={{
-          marginBottom: 8,
-        }}>Выбор проектов</H2Style>
-        <P style={{
-          fontSize: 18,
-        }}>выберите учебный семестр</P>
+    return (
+        <MyProjectStyle>
+            <H2Style style={{
+                marginBottom: 8,
+            }}>Выбор проектов</H2Style>
+            <P style={{
+                fontSize: 18,
+                color: '#B6B6B6'
+            }}>Выберите учебный семестр</P>
 
-        <ButtonSeasonWrapper>
-          {seasons.map((label, idx) => (
-              <div key={idx}>
-                <div onClick={() => setActiveButton(idx)}>
-                  <ButtonSeason label={label} select={idx === activeButton}/>
-                </div>
-              </div>
-          ))}
-        </ButtonSeasonWrapper>
-        {data.checkProject ? <></> : <><Selector type={'role'}
-                                                 width={'356px'}
-                                                 margin={'10px'}
-                                                 labelSelector={'Роль в команде*'}
-                                                 options={[
-                                                   'Team Lead', 'UI/UX-дизайнер', 'Game-дизайнер', 'Unity-разработчик', 'Художник',
-                                                   'UE-разработчик',
-                                                 ]}
-        />
-          <P>Создать команду может только Team Lead</P>
-        </>}
+            <ButtonSeasonWrapper>
+                {seasons.map((season, idx) => (
+                    <div key={idx}>
+                        <div onClick={() => setActiveButton(idx)}>
+                            <ButtonSeason disabled={season['disabled']} label={season['label']} select={idx === activeButton}/>
+                        </div>
+                    </div>
+                ))}
+            </ButtonSeasonWrapper>
+            {data.checkProject ? <></> : <><Selector type={'role'}
+                                                     width={'356px'}
+                                                     margin={'10px'}
+                                                     labelSelector={'Роль в команде*'}
+                                                     options={[
+                                                         'Team Lead', 'UI/UX-дизайнер', 'Game-дизайнер', 'Unity-разработчик', 'Художник',
+                                                         'UE-разработчик',
+                                                     ]}
+            />
+                <P>Создать команду может только Team Lead</P>
+            </>}
 
 
-        {data.checkProject ? <CheckedProjectTeamBlock/> : (data.role ? data.role === 'Team Lead' ? <CreateTeamBlock/> :
-            <TeamBlock/> : <></>)}
+            {data.checkProject ? <CheckedProjectTeamBlock/> : (data.role ? data.role === 'Team Lead' ?
+                <CreateTeamBlock/> :
+                <TeamBlock/> : <></>)}
 
-      </MyProjectStyle>
-  )
+        </MyProjectStyle>
+    )
 }
 
 const ButtonSeasonWrapper = styled.div`
-  display: flex;
-  gap: 24px;
-  margin-bottom: 47px;
-  flex-wrap: wrap;
+    display: flex;
+    gap: 24px;
+    margin-bottom: 47px;
+    flex-wrap: wrap;
 `
 
 const ButtonSeasonStyle = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 166px;
-  height: 44px;
-  border: 1px solid #5A9DF5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 166px;
+    height: 44px;
+    border: 1px solid #5A9DF5;
 
-  transition: background-color 0.1s ease-in-out;
+    transition: background-color 0.1s ease-in-out;
 
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
 
-  cursor: pointer;
+    cursor: pointer;
 
-  -webkit-border-radius: 3px;
-  -moz-border-radius: 3px;
-  border-radius: 3px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
 
-  &:disabled {
-    color: rgba(208, 230, 238, 0.5);
-    border-color: rgba(208, 230, 238, 0.5);
-    cursor: unset;
-  }
+    &:disabled {
+        color: rgba(208, 230, 238, 0.5);
+        border-color: rgba(208, 230, 238, 0.5);
+        cursor: unset;
+    }
 `
 
 const MyProjectStyle = styled.div`
@@ -123,13 +142,13 @@ const MyProjectStyle = styled.div`
 `
 
 const P = styled.div`
-  margin-bottom: 47px;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 17px;
+    margin-bottom: 47px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
 
-  color: rgba(208, 230, 238, 0.94);
+    color: rgba(208, 230, 238, 0.94);
 `
 
 export default MyProjects
