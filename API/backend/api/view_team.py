236b38f3b8c3c9e11a2142b5db82206de0b04f17
Kirill <k890484878789@gmail.com>
2024-04-23 21:02:51 +0500

    Добавил модуль "пользователь" и "проект" в модуль "команда"

--- API/backend/api/view_team.py
+++ API/backend/api/view_team.py
@@ -59,3 +59,12 @@ class TeamView(GenericAPIView):
         team.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    def post(self, request, id, username):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
--- API/backend/team/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('', TeamsView.as_view()),
-    path('<int:id>/', TeamView.as_view())
-]
-
