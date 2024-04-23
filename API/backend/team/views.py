22184a74511c949938c8765d26912967ab910bb7
Kirill <k890484878789@gmail.com>
2024-04-23 19:16:10 +0500

    полностью добавить crud-операции для команды

--- API/backend/team/views.py
+++ API/backend/team/views.py
@@ -1,34 +1,54 @@
 from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from .models import Team
 from .serializers import TeamSerializer
 
 
-class TeamsView(APIView):
+class TeamsView(GenericAPIView):
+    serializer_class = TeamSerializer
+
     def get(self, request):
         teams = Team.objects.all()
-        serializer = TeamSerializer(teams, many=True)
+        serializer = self.serializer_class(teams, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = TeamSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request):
+        Team.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-class TeamView(APIView):
+
+class TeamView(GenericAPIView):
+    serializer_class = TeamSerializer
+
     def get(self, request, id):
         try:
             team = Team.objects.get(id=id)
         except Team.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = TeamSerializer(team)
+        serializer = self.serializer_class(team)
         return Response(serializer.data)
+
+    def put(self, request, id):
+        try:
+            team = Team.objects.get(id=id)
+        except Team.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(team, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
         try:
